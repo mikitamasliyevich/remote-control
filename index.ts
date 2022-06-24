81eebfb434fd65a5a33448060779ac3516601e0a
Mikita Masliyevich <m.masliyevich@godeltech.com>
2022-06-24 18:40:25 +0200

    feat: add screen image

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
-import Jimp from 'jimp';
 import {httpServer} from './src/http_server/index';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream  } from 'ws';
 import { drawCircle, drawSquare, drawRectangle } from './src/drawing/index'
 import { TCoordinates } from './src/types';
 import { MOUSE_UP, MOUSE_DOWN, MOUSE_LEFT, 
-  MOUSE_RIGHT, MOUSE_POSITION, DRAW_CIRCLE, DRAW_SQUARE, DRAW_RECTANGLE, PRNT_SCRN
+  MOUSE_RIGHT, MOUSE_POSITION, DRAW_CIRCLE, 
+  DRAW_SQUARE, DRAW_RECTANGLE, PRNT_SCRN
 } from './src/utils/helpers';
 import { mouseUp, mouseDown, mouseLeft, mouseRight } from './src/navigation/index';
+import {imageController} from "./src/printScreen/index"
 
 const HTTP_PORT: number | string = process.env.PORT || 3000;
 const WEB_SOCKET_PORT: number | string = process.env.WEB_SOCKET_PORT || 8080;
@@ -19,36 +20,38 @@ httpServer.listen(HTTP_PORT);
 const wss = new WebSocketServer({ port: +WEB_SOCKET_PORT}, () => console.log(`Server started on ${WEB_SOCKET_PORT}`));
 
 wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data: any) {
-      const input = data.toString();
+  const duplex = createWebSocketStream(ws, { encoding: 'utf8',  decodeStrings: false });
+  duplex.on('data', (chunk) => {
+      const input = chunk.toString();
       const values: number = parseInt(input.match(/\d+/));
-      const { x, y }: TCoordinates= robot.getMousePos();
-      
+      const { x, y }: TCoordinates = robot.getMousePos()
+
       if(input.includes(MOUSE_UP)) {
-        ws.send(MOUSE_UP)
+        duplex.write(MOUSE_UP)
         mouseUp(values)
       } else if(input.includes(MOUSE_DOWN)){
-        ws.send(MOUSE_DOWN)
+        duplex.write(MOUSE_DOWN)
         mouseDown(values)
       } else if( input.includes(MOUSE_LEFT)) {
-        ws.send(MOUSE_LEFT)
+        duplex.write(MOUSE_LEFT)
         mouseLeft(values)
       } else if(input.includes(MOUSE_RIGHT)) {
-        ws.send(MOUSE_RIGHT)
+        duplex.write(MOUSE_RIGHT)
         mouseRight(values)
       } else if(input.includes(MOUSE_POSITION)) {
-        ws.send(`${MOUSE_POSITION},x=${x}px,y=${y}px`)
+        duplex.write(`${MOUSE_POSITION} ${x}px,${y}px`)
       } else if(input.includes(DRAW_CIRCLE)) {
-        ws.send(DRAW_CIRCLE);
+        duplex.write(DRAW_CIRCLE);
         drawCircle(values)
       } else if(input.includes(DRAW_SQUARE)) {
-        ws.send(DRAW_SQUARE)
-        drawSquare(values)
+        duplex.write(DRAW_SQUARE);
+        drawSquare(values);
       } else if(input.includes(DRAW_RECTANGLE)) {
-        ws.send(DRAW_RECTANGLE)  
-        drawRectangle(input)
+        duplex.write(DRAW_RECTANGLE);  
+        drawRectangle(input);
       } else if(input.includes(PRNT_SCRN)) {
-          ws.send(PRNT_SCRN)
+        duplex.write(PRNT_SCRN)
+        imageController(ws)
       } else {
         console.log('Please check your conditions')
       }
--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -2,16 +2,23 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 
-export const httpServer = http.createServer(function (req, res) {
+export const httpServer: http.Server = http.createServer(function (req: http.IncomingMessage, res: http.ServerResponse) {
+    let currentData: string = "";
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+    const FSstream: fs.ReadStream = fs.createReadStream(file_path);
+
+    FSstream.on("data", (data: string) => {
+        currentData += data;
+      });
+
+    FSstream.on("end", () => {
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.end(currentData);
+      });
+
+    FSstream.on("error", (err: Error) => {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+      });
 });
